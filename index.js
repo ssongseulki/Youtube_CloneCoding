faa62ee8c8157aa56a79ab5f67bea769a64ba691
ssongseulki <ssongseulki@naver.com>
2020-12-17 15:35:57 +0900

    javascript_add

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ moreBtn.addEventListener("click", () => {
   moreBtn.classList.toggle("clicked");
   title.classList.toggle("clamp");
 });
+
+
+function upBtnClick() {
+  const upBtn = document.querySelector("#upNum");
+  const upNum = Number(upBtn.innerHTML);
+  upBtn.innerHTML = upNum + 1 ;
+};
+
+function downBtnClick() {
+  const downBtn = document.querySelector("#downNum");
+  const downNum = Number(downBtn.innerHTML);
+  downBtn.innerHTML = downNum + 1 ;
+};
+
+function saveBtnClick() { 
+  alert("저장되었습니다.");
+}
+
+function reportBtnClick() {
+ const inputReport =  prompt("신고내용을 입력하세요.");
+  
+  if (inputReport != null) {
+    alert("신고되었습니다.");
+  };
+}
